490bbecc62eab2832786d4d1404a852cc3c9160d
Flero <flero@flero.dev>
2023-11-10 00:36:40 +1030

    feat(StartupLogo): module to change minecraft's startup logo

--- src/main/java/dev/flero/bismuth/mixin/MinecraftClientMixin.java
+++ src/main/java/dev/flero/bismuth/mixin/MinecraftClientMixin.java
@@ -1,16 +1,32 @@
 package dev.flero.bismuth.mixin;
 
 import dev.flero.bismuth.modules.GameTitle;
+import dev.flero.bismuth.modules.StartupLogo;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.DefaultResourcePack;
+import net.minecraft.util.Identifier;
 import org.lwjgl.opengl.Display;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
     @Redirect(method = "setPixelFormat", at = @At(value = "INVOKE", target = "org/lwjgl/opengl/Display.setTitle(Ljava/lang/String;)V"))
     public void setTitle(String title) {
         Display.setTitle(GameTitle.getTitle(title));
+    }
+
+    @Redirect(method = "loadLogo", at = @At(value = "INVOKE", target = "net/minecraft/resource/DefaultResourcePack.open(Lnet/minecraft/util/Identifier;)Ljava/io/InputStream;"))
+    public InputStream setLogo(DefaultResourcePack instance, Identifier id) throws IOException {
+        if (!StartupLogo.isEnabled) {
+            return instance.open(id);
+        }
+
+        id = StartupLogo.logo;
+        return instance.open(id);
     }
 }
---
+++ src/main/java/dev/flero/bismuth/modules/StartupLogo.java
@@ -0,0 +1,8 @@
+package dev.flero.bismuth.modules;
+
+import net.minecraft.util.Identifier;
+
+public class StartupLogo {
+    public static boolean isEnabled = true;
+    public static Identifier logo = new Identifier("bismuth", "textures/gui/title/bismuth.png");
+}
