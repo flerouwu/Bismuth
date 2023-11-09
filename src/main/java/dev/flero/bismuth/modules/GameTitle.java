b90bf933ea9f53bbc7fadd2b87c6847db7a0f3cc
Flero <flero@flero.dev>
2023-11-09 22:18:57 +1030

    fix(GameTitle): adjust title to show version

--- src/main/java/dev/flero/bismuth/modules/GameTitle.java
+++ src/main/java/dev/flero/bismuth/modules/GameTitle.java
@@ -1,11 +1,13 @@
 package dev.flero.bismuth.modules;
 
+import dev.flero.bismuth.BismuthMod;
+
 public class GameTitle {
     public static boolean isEnabled = true;
 
     public static String getTitle(String original) {
         if (isEnabled) {
-            return "Bismuth by Flero | " + original;
+            return String.format("Bismuth %s - %s", BismuthMod.version, original);
         } else {
             return original;
         }
