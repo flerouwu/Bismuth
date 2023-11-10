e50cd15f29afd1e5c3d16853ee99af3e10f8c982
Flero <flero@flero.dev>
2023-11-10 15:36:50 +1030

    feat(GameTitle): add template to allow for custom titles

--- src/main/java/dev/flero/bismuth/modules/GameTitle.java
+++ src/main/java/dev/flero/bismuth/modules/GameTitle.java
@@ -4,14 +4,20 @@ import dev.flero.bismuth.BismuthMod;
 import dev.flero.bismuth.config.ConfigHolder;
 import dev.flero.bismuth.config.ConfigValue;
 
+import java.text.MessageFormat;
+
 @ConfigHolder(name = "modules/game_title")
 public class GameTitle {
     @ConfigValue
     public static boolean isEnabled = true;
 
+    @ConfigValue
+    public static String template = "Bismuth {0} - {1}";
+
     public static String getTitle(String original) {
         if (isEnabled && BismuthMod.container != null) {
-            return String.format("Bismuth %s - %s", BismuthMod.container.getMetadata().getVersion().getFriendlyString(), original);
+            MessageFormat format = new MessageFormat(template);
+            return format.format(new Object[]{BismuthMod.container.getMetadata().getVersion().getFriendlyString(), original});
         } else {
             return original;
         }
