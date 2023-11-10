d0095171c0b330c38d97688fb8d58641f7e54b91
Flero <flero@flero.dev>
2023-11-10 11:29:53 +1030

    fix(GameTitle): fixed refactor BismuthMod

--- src/main/java/dev/flero/bismuth/modules/GameTitle.java
+++ src/main/java/dev/flero/bismuth/modules/GameTitle.java
@@ -1,13 +1,17 @@
 package dev.flero.bismuth.modules;
 
 import dev.flero.bismuth.BismuthMod;
+import dev.flero.bismuth.config.ConfigHolder;
+import dev.flero.bismuth.config.ConfigValue;
 
+@ConfigHolder(name = "modules/game_title")
 public class GameTitle {
+    @ConfigValue
     public static boolean isEnabled = true;
 
     public static String getTitle(String original) {
         if (isEnabled) {
-            return String.format("Bismuth %s - %s", BismuthMod.version, original);
+            return String.format("Bismuth %s - %s", BismuthMod.container.getMetadata().getVersion().getFriendlyString(), original);
         } else {
             return original;
         }
